7962f09794be71d4ed70b75ba0cf7a9103bf928f
romintomasetti <romin.tomasetti@gmail.com>
2025-11-05 17:07:12 +0000

    get rid of package typeguard dependency
    but keep it for testing
    
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name             = 'hwloc-xml-parser',
     version          = '0.1',
-    install_requires = [
-        'typeguard',
-    ],
     packages         = find_packages(),
     license          = 'MIT',
     url              = 'https://github.com/uliegecsm/hwloc-xml-parser'
